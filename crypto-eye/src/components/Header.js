fdcf5ef8b13d01e2b6ac092d82eb7fece78a2346
MiguelRemedios <1919716@brunel.ac.uk>
2021-12-09 20:57:09 +0000

    darkTheme

--- crypto-eye/src/components/Header.js
+++ crypto-eye/src/components/Header.js
@@ -6,7 +6,9 @@ import {
   MenuItem,
   Typography,
   makeStyles,
+  ThemeProvider,
 } from "@material-ui/core";
+import { createTheme } from "@material-ui/core/styles";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -23,27 +25,40 @@ const useStyles = makeStyles({
 const Header = () => {
   const classes = useStyles();
   const navigate = useNavigate();
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
   function handleClick() {
     navigate("/");
   }
+
   return (
-    <AppBar color="transparent" position="static">
-      <Container>
-        <Toolbar>
-          <Typography onClick={handleClick} className={classes.title}>
-            CRYPTO EYE
-          </Typography>
-          <Select
-            variant="outlined"
-            style={{ width: 100, height: 40, marginLeft: 15 }}
-          >
-            <MenuItem value={"EUR"}>EUR</MenuItem>
-            <MenuItem value={"USD"}>USD</MenuItem>
-            <MenuItem value={"GBP"}>GBP</MenuItem>
-          </Select>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography onClick={handleClick} className={classes.title}>
+              CRYPTO EYE
+            </Typography>
+            <Select
+              variant="outlined"
+              style={{ width: 100, height: 40, marginLeft: 15 }}
+            >
+              <MenuItem value={"EUR"}>EUR</MenuItem>
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"GBP"}>GBP</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
   );
 };
 
