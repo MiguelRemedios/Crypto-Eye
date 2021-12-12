3d45e8de6b64081abc79763ca2c262695a0d7c2f
MiguelRemedios <1919716@brunel.ac.uk>
2021-12-12 12:30:51 +0000

    Banner addition + Banner Component + Homepage update

---
+++ crypto-eye/src/components/Banner/Banner.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Typography, Container, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(() => ({
+  banner: {
+    backgroundImage: "url(./banner.jpg)",
+  },
+  bannerContent: {
+    height: 400,
+    display: "flex",
+    flexDirection: "column",
+    paddingTop: 25,
+    justifyContent: "space-around",
+  },
+  tagline: {
+    display: "flex",
+    height: "40%",
+    flexDirection: "column",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+}));
+
+const Banner = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.banner}>
+      <Container className={classes.bannerContent}>
+        <div className={classes.tagline}>
+          <Typography
+            variant="h2"
+            style={{
+              fontWeight: "bold",
+              marginBottom: 15,
+              fontFamily: "Montserrat",
+            }}
+          >
+            CRYPTO EYE
+          </Typography>
+          <Typography
+            variant="subtitle2"
+            style={{
+              color: "darkgrey",
+              textTransform: "capitalize",
+              fontFamily: "Montserrat",
+            }}
+          >
+            All Information About Crypto Currencies
+          </Typography>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Banner;
