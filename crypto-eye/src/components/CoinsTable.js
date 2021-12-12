f261ebc54df75a37ecdcd13bc7224e86bd6b3993
MiguelRemedios <1919716@brunel.ac.uk>
2021-12-12 16:04:04 +0000

    Search input box and title

--- crypto-eye/src/components/CoinsTable.js
+++ crypto-eye/src/components/CoinsTable.js
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { CoinList } from "../config/api";
+import { CryptoState } from "../CryptoContext";
+import axios from "axios";
+import {
+  Container,
+  createTheme,
+  TextField,
+  ThemeProvider,
+  Typography,
+} from "@material-ui/core";
 
 const CoinsTable = () => {
-  return <div>CoinsTable</div>;
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
+
+  const { currency } = CryptoState();
+
+  const fetchCoins = async () => {
+    setLoading(true);
+    const { data } = await axios.get(CoinList(currency));
+    setCoins(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    // eslint-disable-next-line
+    fetchCoins();
+    // eslint-disable-next-line
+  }, [currency]);
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Container style={{ textAlign: "center" }}>
+        <Typography
+          variant="h5"
+          style={{
+            margin: 18,
+            fontFamily: "Montserrat",
+            fontWeight: "lighter",
+          }}
+        >
+          CRYPTOCURRENCY PRICES BY MARKET CAP
+        </Typography>
+        <TextField
+          label="Search for a Cryptocurrency..."
+          variant="outlined"
+          style={{ marginBottom: 20, width: "100%" }}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </Container>
+    </ThemeProvider>
+  );
 };
 
 export default CoinsTable;
