380ed5c5f7da8bc1494187418394352edb40022a
MiguelRemedios <1919716@brunel.ac.uk>
2021-12-13 21:50:12 +0000

    Table Insertion + Table Headers + Small adjustments

--- crypto-eye/src/components/CoinsTable.js
+++ crypto-eye/src/components/CoinsTable.js
@@ -5,6 +5,12 @@ import axios from "axios";
 import {
   Container,
   createTheme,
+  LinearProgress,
+  Table,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
   TextField,
   ThemeProvider,
   Typography,
@@ -38,15 +44,19 @@ const CoinsTable = () => {
       type: "dark",
     },
   });
+
+  const columns = ["Coin", "Price", "24 Change", "Market Cap"];
+
   return (
     <ThemeProvider theme={darkTheme}>
       <Container style={{ textAlign: "center" }}>
         <Typography
-          variant="h5"
+          variant="h4"
           style={{
             margin: 18,
             fontFamily: "Montserrat",
-            fontWeight: "lighter",
+            marginTop: 30,
+            marginBottom: 30,
           }}
         >
           CRYPTOCURRENCY PRICES BY MARKET CAP
@@ -57,6 +67,32 @@ const CoinsTable = () => {
           style={{ marginBottom: 20, width: "100%" }}
           onChange={(e) => setSearch(e.target.value)}
         />
+
+        <TableContainer>
+          {loading ? (
+            <LinearProgress style={{ backgroundColor: "gold" }} />
+          ) : (
+            <Table>
+              <TableHead style={{ backgroundColor: "#EEBC1D" }}>
+                <TableRow>
+                  {columns.map((header) => (
+                    <TableCell
+                      style={{
+                        color: "black",
+                        fontWeight: "700",
+                        fontFamily: "Montserrat",
+                      }}
+                      key={header}
+                      align={header === "Coin" ? "" : "right"}
+                    >
+                      {header}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+            </Table>
+          )}
+        </TableContainer>
       </Container>
     </ThemeProvider>
   );
