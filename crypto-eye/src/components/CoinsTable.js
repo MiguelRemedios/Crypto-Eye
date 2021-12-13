59d797d94d3221500376fedf6f90242bfbbdcd7f
MiguelRemedios <1919716@brunel.ac.uk>
2021-12-13 22:34:44 +0000

    Coin Column Table Cells addition

--- crypto-eye/src/components/CoinsTable.js
+++ crypto-eye/src/components/CoinsTable.js
@@ -6,7 +6,9 @@ import {
   Container,
   createTheme,
   LinearProgress,
+  makeStyles,
   Table,
+  TableBody,
   TableCell,
   TableContainer,
   TableHead,
@@ -15,11 +17,30 @@ import {
   ThemeProvider,
   Typography,
 } from "@material-ui/core";
+import { useNavigate } from "react-router-dom";
+
+const useStyles = makeStyles({
+  row: {
+    backgroundColor: "#16171a",
+    cursor: "pointer",
+    "&:hover": {
+      backgroundColor: "#131111",
+    },
+    fontFamily: "Montserrat",
+  },
+  pagination: {
+    "& .MuiPaginationItem-root": {
+      color: "gold",
+    },
+  },
+});
 
 const CoinsTable = () => {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(false);
   const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+  const classes = useStyles();
 
   const { currency } = CryptoState();
 
@@ -46,6 +67,14 @@ const CoinsTable = () => {
   });
 
   const columns = ["Coin", "Price", "24 Change", "Market Cap"];
+
+  const handleSearch = () => {
+    return coins.filter(
+      (coin) =>
+        coin.name.toLowerCase().includes(search) ||
+        coin.symbol.toLowerCase().includes(search)
+    );
+  };
 
   return (
     <ThemeProvider theme={darkTheme}>
@@ -90,6 +119,47 @@ const CoinsTable = () => {
                   ))}
                 </TableRow>
               </TableHead>
+              <TableBody>
+                {handleSearch().map((row) => {
+                  const profit = row.price_change_percentage_24h > 0;
+                  return (
+                    <TableRow
+                      onClick={() => navigate(`/coins/${row.id}`)}
+                      className={classes.row}
+                      key={row.name}
+                    >
+                      <TableCell
+                        component="th"
+                        scope="row"
+                        style={{
+                          display: "flex",
+                          gap: 15,
+                        }}
+                      >
+                        <img
+                          src={row?.image}
+                          alt={row.name}
+                          height="50"
+                          style={{ marginBottom: 10 }}
+                        />
+                        <div
+                          style={{ display: "flex", flexDirection: "column" }}
+                        >
+                          <span
+                            style={{
+                              textTransform: "uppercase",
+                              fontSize: 22,
+                            }}
+                          >
+                            {row.symbol}
+                          </span>
+                          <span style={{ color: "darkgrey" }}>{row.name}</span>
+                        </div>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
             </Table>
           )}
         </TableContainer>
