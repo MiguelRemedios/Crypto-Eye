89db7bb34d9401500bffd799f32a35a8ca2d8780
MiguelRemedios <1919716@brunel.ac.uk>
2021-12-15 14:42:33 +0000

    CoinInfo update

--- crypto-eye/src/components/CoinInfo.js
+++ crypto-eye/src/components/CoinInfo.js
@@ -1,7 +1,64 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import { CryptoState } from "../CryptoContext";
+import axios from "axios";
+import { HistoricalChart } from "../config/api";
+import { Line } from "react-chartjs-2";
+import {
+  CircularProgress,
+  createTheme,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core";
 
-const CoinInfo = () => {
-  return <div>Coin Info</div>;
+const useStyles = makeStyles((theme) => ({
+  container: {
+    width: "75%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 25,
+    padding: 40,
+    [theme.breakpoints.down("md")]: {
+      width: "100%",
+      marginTop: 0,
+      padding: 20,
+      paddingTop: 0,
+    },
+  },
+}));
+
+const CoinInfo = ({coin}) => {
+
+  const [historicData, setHistoricData] = useState();
+  const [days, setDays] = useState(1);
+  const { currency } = CryptoState();
+
+  const classes = useStyles();
+
+  const fetchHistoricData = async () => {
+    const { data } = await axios.get(HistoricalChart(coin.id, days, currency));
+
+    setHistoricData(data.prices);
+  };
+
+  useEffect(() => {
+    fetchHistoricData();
+    // eslint-disable-next-line
+  }, [days]);
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+    },
+  });
+
+  return (
+  <ThemeProvider theme={darkTheme}><div>Coin Info</div> <div></div></ThemeProvider>);
 };
+
+
 
 export default CoinInfo;
